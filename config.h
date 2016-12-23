48e26930d7f031f34aa5ef040eddc83609b87cf8
Marcus A. Link <Manupool@users.noreply.github.com>
2016-12-23 11:47:54 +0100

    Update config.h

--- config.h
+++ config.h
@@ -49,7 +49,7 @@
 #define CMD_STATUS_REPORT '?'
 #define CMD_FEED_HOLD '!'
 #define CMD_CYCLE_START '~'
-#define CMD_RESET 0x18 // ctrl-x.
+#define CMD_RESET 'qq' // ctrl-x.
 #define CMD_SAFETY_DOOR '@'
 
 // If homing is enabled, homing init lock sets Grbl into an alarm state upon power up. This forces
